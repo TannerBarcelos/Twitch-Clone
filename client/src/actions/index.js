2bacdb43705d8deb4f3373b396a5d80da6bbe69b
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-06-16 00:26:14 -0700

    action crerators made.study the whole idea here

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,7 +1,16 @@
 import streams from '../apis/streams'; //axios instance definer to make requests to backend [this type of design is used a lot..study it and get used to it]
 
 //technique: make string constants in a types.js file for the action types and use them as vars in our action creators to make life easier
-import {SIGN_IN, SIGN_OUT} from './types';
+//and avoid bugs of typos in our action types
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './types';
 
 //notice no returned payload: these actions are simply used for booleans and signing in a user with auth
 export const signIn = id => {
@@ -19,7 +28,65 @@ export const signOut = () => {
 //async so need thunk and explicit dispatch: send a post request to the db.json file passing it formValues (given in the actionCreator call in the createStream component)
 //notice though, we have no reducer for this.
 export const createStream = formValues => async dispatch => {
-  streams.post ('/streams', formValues);
+  const response = await streams.post ('/streams', formValues);
+  //dispatch create stream as an action type and the axios response object from the post
+  //remember async action creators need to dispatch explicitely instead of return
+  dispatch ({
+    type: CREATE_STREAM,
+    payload: response.data,
+  });
   //we imported streams which is our axios instance from apis folder which will send a post request to the 3001 localhost,
   //at the route of /streams [this type of stuff would be different with express kinda. see the ecommerce app]
 };
+
+//action creator to fetch all the streams
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get ('/streams');
+  dispatch ({
+    type: FETCH_STREAMS,
+    payload: response.data,
+  });
+};
+
+//takes in the stream id to fetch one stream
+export const fetchSteam = id => async dispatch => {
+  const response = await streams.get (`/streams/${id}`);
+  dispatch ({
+    type: FETCH_STREAM,
+    payload: response.data,
+  });
+};
+
+//edits a stream by given id and the form values entered in a form used to update stream info
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put (`/streams/${id}`, formValues);
+  dispatch ({
+    type: EDIT_STREAM,
+    payload: response.data,
+  });
+};
+
+//gets stream to delete by id
+export const deleteStream = id => async dispatch => {
+  await streams.delete (`/streams/${id}`);
+  dispatch ({
+    type: DELETE_STREAM,
+    payload: id,
+  });
+};
+
+/**
+ * AGAIN: REMEMBER,
+ * 
+ * If we make async actions like network requests, we need thunk middleware
+ * 
+ * but, the main thing here is that action creators are simple methods made to export and use in components
+ * that basically can take data in, and dispatch to the store to make changes to state. we use connect() to send these actions
+ * to the store in which EVERY reducer we have WILL see ALL actions in which we send! It is sort of magic but not
+ * 
+ * We simply need to map state to that component prop to use that data if we want to see data change in our state in the UI
+ * 
+ * all action creators that make requests like we do in almost every action creator here must not return an action/payload to dispatch
+ * but must explicitely return an asyn dispatch and await data response and then dispatch the type/payload instead of the implicit 
+ * dispatch thats done for us in component level
+ */
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,5 +1,9 @@
 export const SIGN_IN = 'SIGN_IN';
 export const SIGN_OUT = 'SIGN_OUT';
-
+export const CREATE_STREAM = 'CREATE_STREAM';
+export const FETCH_STREAMS = 'FETCH_STREAMS';
+export const FETCH_STREAM = 'FETCH_STREAM';
+export const DELETE_STREAM = 'DELETE_STREAM';
+export const EDIT_STREAM = 'EDIT_STREAM';
 //this file almost acts like a config file for all action type constant string definitions we can use in our action creators and reducers
 //named export === destructuring needed
