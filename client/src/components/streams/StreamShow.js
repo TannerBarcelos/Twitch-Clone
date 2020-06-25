f9dcf3693b118fa647afdb59b961a1731727a955
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2020-06-25 12:59:43 -0700

    main app done!

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 
+// streaming dependency
+import flv from 'flv.js';
+
 // react-redux/redux stuff
 import {connect} from 'react-redux';
 
@@ -7,11 +10,48 @@ import {connect} from 'react-redux';
 import {fetchStream} from '../../actions';
 
 class StreamShow extends Component {
-  componentDidMount () {
-    this.props.fetchStream (this.props.match.params.id); // the match.params.id stuff is from google auth i think? will need to check
+  constructor (props) {
+    super (props);
+
+    // create a reference to the video element in the DOM with a ref -> this is how to reference html when we are not actually using html in JSX (see docs for the flv library: we are doing this becausue of that)
+    this.videoRef = React.createRef ();
   }
+
+  // runs immediately on mount: fetch stream and try to build th eplay. when the video player responds with video, the compontn didUpdate! therefore
+  // use another lifecyle method called 'componentDidUpdat' to re0render the componnet and build that player
+  componentDidMount () {
+    const {id} = this.props.match.params;
+    this.props.fetchStream (id); // the match.params.id stuff is from google auth i think? will need to check
+    this.buildPlayer (); //build the player [will show it or not depending on the asyncronous response]
+  }
+
+  // component updated: build the actual player [re-redners the ui after an update is invoked, automatically, like the player being loaded]
+  componentDidUpdate () {
+    this.buildPlayer ();
+  }
+
+  // cleanup the video resource off the UI when the stream is ended
+  componentWillUnmount () {
+    // stop attempting to stream and detach from the UI
+    this.player.destroy ();
+  }
+
+  buildPlayer () {
+    if (this.player || !this.props.stream) return;
+
+    const {id} = this.props.match.params;
+    this.player = flv.createPlayer ({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+
+    this.player.attachMediaElement (this.videoRef.current);
+    this.player.load ();
+    this.player.play (); // auto-play
+  }
+
   render () {
-    // UX Thing: if the fetch to streams is  still loading, show this to look nice
+    // do nothing till the stream is fetched and ready to mount the video to the screen
     if (!this.props.stream) {
       return (
         <div>
@@ -25,6 +65,8 @@ class StreamShow extends Component {
 
     return (
       <div>
+        {/**controls by default is true, so, we can negate writing {true} as react assumes true for boolean props */}
+        <video ref={this.videoRef} style={{width: '100%'}} controls />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -5,10 +5,6 @@ import {reducer as formReducer} from 'redux-form'; //named it formReducer to mak
 import authReducer from './authReducer';
 import streamsReducer from './streamReducer';
 
-//reducer alias will be the key of the global store to reference when mapping state to props -> so i every component, when mapping state to props,
-//we can use the state object in the params of mapState.. to access the key of the specific reducer we want to use and extract state to props
-//REMEMBER: TO CHANGE STATE, WE NEED TO USE A DISPATCHER TO DO THAT! THAT IS THE SECOND PARAM OF COONECT() WHICH DISPATCHES ALL NEEEDED ACTION CREATORS WE WANT TO USE
-//IN ANY COMPONENT, TO OUR STORE [WHICH IS ALL COMBINED WITH COMBINE REDUCER HERE BUT GETS SHOVED TO ALL REDUCERS]
 export default combineReducers ({
   auth: authReducer, //for all authentication state [we refer to this in compoentns mapstate as state.auth.stateObjectKey in that file]
   form: formReducer, //redux form is a reducer itself: it does all the work so we just pass a reference to it from the import and have to name it form (go to StreamCreate to see why -> we need to 'map' the form data to the component with the form)
@@ -17,5 +13,5 @@ export default combineReducers ({
 
 /**
  * redux form reducer is created for us when installing and using redux-form. super cool
-  the key name mUST be called form, though. must import the named export by the exact name as reducer [we can alias it with 'as' and some shorthand name]
+  the key name MUST be called form, though and the import from 'redux'form' should be {reducer} or to make it nicer {reducer as formReducer}
  */
